54aed2b040f2f96e56104f26a9c7e9e32609a722
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-02-05 17:02:43 -0800

    Enemy Jump

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        Vector2 Enemy = collision.contacts[0].point;
+        Vector2 Player = collider.bounds.center;
+        float Width = this.GetComponent<Collider2D>().bounds.size.x;
+        
+        if (Player.y > Enemy.y && Player.x < Enemy.x + Width / 2 && Player.x > Enemy.x - Width / 2)
+        {
+            Debug.Log("Hit");
+            GameObject.Find("Player").GetComponent<Rigidbody2D>().AddForce(Vector2.up*15000);
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/playerbounce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbounce : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        Vector2 Player = collision.contacts[0].point;
+        Vector2 Enemy = collider.bounds.center;
+        float Width = this.GetComponent<Collider2D>().bounds.size.x;
+        if (collision.gameObject.name == "Enemy")
+        {
+            if (Player.y > Enemy.y && Player.x < Enemy.x + Width / 2 && Player.x > Enemy.x - Width / 2)
+            {
+                Debug.Log("Hit");
+                this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 15000);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
