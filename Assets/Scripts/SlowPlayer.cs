6749548d5c49ff3b753787069e9a5e539da26e2b
jasonthaisy <jtsy@uci.edu>
2017-02-06 19:10:46 -0800

    Finished slowing gel, working on trap

---
+++ Assets/Scripts/SlowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowPlayer : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            modSpeed(other.GetComponent<PlayerController>(), 4f);
+            modJump(other.GetComponent<PlayerController>(), 0);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            modSpeed(other.GetComponent<PlayerController>(), 14f);
+            modJump(other.GetComponent<PlayerController>(), 14f);
+        }
+    }
+
+    void modSpeed(PlayerController player, float newSpeed)
+    {
+        player.speed = newSpeed;
+    }
+
+    void modJump(PlayerController player, float newJump)
+    {
+        player.jump = newJump;
+    }
+}
---
+++ Assets/TrapPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlayer : MonoBehaviour
+{
+    private Collider2D trap;
+    //private GameObject player;
+    float timeLeft = 10.0f;
+
+    void Start()
+    {
+        trap = GetComponent<Collider2D>();
+        //player = GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        //if (trap.IsTouching(player.GetComponent<Collider2D>()))
+        //{
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                //modSpeed(player.GetComponent<PlayerController>(), 14f);
+                //modJump(player.GetComponent<PlayerController>(), 14f);
+            }
+        //}
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            modSpeed(other.GetComponent<PlayerController>(), 0);
+            modJump(other.GetComponent<PlayerController>(), 0); 
+        }
+    }
+
+    void modSpeed(PlayerController player, float newSpeed)
+    {
+        player.speed = newSpeed;
+    }
+
+    void modJump(PlayerController player, float newJump)
+    {
+        player.jump = newJump;
+    }
+}
