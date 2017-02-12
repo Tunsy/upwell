efd0f293d4454e87d356bb2f565096e28bed1bd3
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-02-12 12:58:25 -0800

    UI score (Missing score added from pickups)
    Just a timer that checks and stops and shows the score when the game
    over screen is set true. Still need to adjust it to read the
    gamemanager's pickup score.

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -10,15 +10,39 @@ public class EnemyScript : MonoBehaviour {
         Vector2 Enemy = collision.contacts[0].point;
         Vector2 Player = collider.bounds.center;
         float Width = this.GetComponent<Collider2D>().bounds.size.x;
-        
-        if (Player.y > Enemy.y && Player.x < Enemy.x + Width / 2 && Player.x > Enemy.x - Width / 2)
-        {
-            Debug.Log("Hit");
-            GameObject.Find("Player").GetComponent<Rigidbody2D>().AddForce(Vector2.up*15000);
-        }
-        else
-        {
+        float Height = this.GetComponent<Collider2D>().bounds.size.y;
+        int x = 200;
 
+        if (collider.name == "Player")
+        {
+            /*if (Player.y >= Enemy.y && Player.x < Enemy.x + Width / 2 && Player.x > Enemy.x - Width / 2)
+            {
+                GameObject.Find("Player").GetComponent<Rigidbody2D>().AddForce(Vector2.up * 2000);
+                GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity = Vector2.up * 1000;
+            }
+            else*/ if (Player.x > Enemy.x && Player.y < Enemy.y + Height / 2)
+            {
+                GameObject.Find("Player").GetComponent<Rigidbody2D>().AddForce(Vector2.right * 2000);
+                while (x > 0)
+                new WaitForSeconds(1);
+                {
+                    GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    Debug.Log(x);
+                    x -= 1;
+                }
+                x = 200;
+            }
+            else if (Player.x < Enemy.x && Player.y < Enemy.y + Height / 2)
+            {
+                GameObject.Find("Player").GetComponent<Rigidbody2D>().AddForce(Vector2.left * 2000);
+                while (x > 0)
+                {
+                    GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    Debug.Log(x);
+                    x -= 1;
+                }
+                x = 200;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private int score;
+
+    void Start ()
+    {
+        score = 0;
+    }
+
+	void Update ()
+    {
+        score += 1;
+        if(GameObject.Find("Scoretotal") == true)
+        {
+            Text txt = GameObject.Find("Scoretotal").GetComponent<Text>();
+            txt.text = "Score: " + score;
+            Destroy(this);
+        }
+    }
+
+}
