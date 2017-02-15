2cd808796780d156d3e96e0a38b84bb0883aba76
kbuci <krishenb@uci.edu>
2017-02-14 20:50:58 -0800

    added pickup object and script

---
+++ Assets/Scripts/PickupTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupTrigger : MonoBehaviour {
+    private string tag = "Player";
+    public int value = 5;
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == tag)
+        {
+            GameManager.instance.updatePickupScore(value);
+        }
+        Destroy(this);
+    }
+
+    
+}
