887746971c0c0638a59f3b84eca22f1a0f8e3155
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-02-18 19:18:51 -0800

    Adding The Moving Platform and Rocket

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public int x;
+    bool Playertouching = false;
+    float offset;
+
+	void Update ()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, x), transform.position.y);
+        if(Playertouching == true)
+        {
+                offset = GameObject.Find("Player").transform.position.x - transform.position.x;
+                GameObject.Find("Player").transform.position = new Vector3(transform.position.x + offset, GameObject.Find("Player").transform.position.y);
+
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Space))
+            {
+                Playertouching = false;
+            }
+            else
+            {
+                Playertouching = true;
+            }
+        }
+    }
+}
---
+++ Assets/RocketScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        Vector2 Player = GameObject.Find("Player").transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, Player, .05f);
+        //transform.rotation = Quaternion.LookRotation(Player);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
