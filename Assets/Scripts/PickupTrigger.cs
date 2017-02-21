2f99c98f03c3096b2e1e4dec429b2c2f9b3d71e9
kbuci <krishenb@uci.edu>
2017-02-20 20:05:45 -0800

    fixed Destroy for pickup

--- Assets/Scripts/PickupTrigger.cs
+++ Assets/Scripts/PickupTrigger.cs
@@ -6,13 +6,18 @@ public class PickupTrigger : MonoBehaviour {
     private string tag = "Player";
     public int value = 5;
     // Use this for initialization
+    void onStart()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, GameObject.FindGameObjectWithTag(tag).transform.position.z);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == tag)
         {
             GameManager.instance.updatePickupScore(value);
+            Debug.Log(GameManager.instance.getPlayerScore());
         }
-        Destroy(this);
+        Destroy(gameObject);
     }
 
     
