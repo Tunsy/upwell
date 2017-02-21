a6de090ef4cb24fd025a846a3586ab686dff559f
jasonthaisy <jtsy@uci.edu>
2017-02-21 00:40:59 -0800

    Destroyer Updates
    needs refinement

--- Assets/Scripts/LavaMoveUp.cs
+++ Assets/Scripts/LavaMoveUp.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class LavaMoveUp : MonoBehaviour
 {
-
+    public GameObject player;
+    //private Rigidbody2D rb;
     public float upSpeed;
+    public float maxDistance;
+    public float catchUp; //speed to use if player and destroyer are too far apart
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>(); //might use this later for smoother destroyer accel/decel
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x, transform.position.y + (upSpeed * Time.deltaTime));
-        
+        Vector2 destroyerPos = transform.position;
+        Vector2 playerPos = player.transform.position;
+        if ((destroyerPos - playerPos).magnitude > maxDistance)
+        {
+            Debug.Log("catch up");
+            transform.position = new Vector2(transform.position.x, transform.position.y + (catchUp * Time.deltaTime));
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + (upSpeed * Time.deltaTime));
+        }
     }
 }
