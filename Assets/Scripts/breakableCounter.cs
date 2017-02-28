bd4f4dc80d94e85e62dc4ef97420756ac16c9c53
kbuci <krishenb@uci.edu>
2017-02-27 17:37:15 -0800

    modifed collison/trigger of breakable object

--- Assets/Scripts/breakableCounter.cs
+++ Assets/Scripts/breakableCounter.cs
@@ -10,17 +10,18 @@ public class breakableCounter : MonoBehaviour {
     {
        transform.position = new Vector3(transform.position.x, transform.position.y, GameObject.FindGameObjectWithTag("Player").transform.position.z);
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             durability -= 1;
-            if(durability == 0)
+            if (durability == 0)
             {
                 Destroy(gameObject);
             }
         }
     }
+   
 
     
 }
