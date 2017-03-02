4684342d2463b4cf2e71bab6c91acb162f5ac36a
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-03-01 17:55:58 -0800

    Trying to avoid merging conflicts

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -11,7 +11,7 @@ public class MovingPlatform : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(Mathf.PingPong(Time.time, x), transform.position.y);
-        offset = GameObject.Find("Player").transform.position.x - transform.position.x;
+
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -20,14 +20,23 @@ public class MovingPlatform : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Space))
             {
-                
+                offset = GameObject.Find("Player").transform.position.x - transform.position.x;
                 other.transform.parent = null;
                 other.transform.position = new Vector3(transform.position.x + offset, other.transform.position.y);
+                
             }
             else
             {
                 other.transform.parent = this.transform;
             }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
         }
     }
 }
