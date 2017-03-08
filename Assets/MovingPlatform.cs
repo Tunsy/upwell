bccc47ace62bff3d212e362688495305e25ddd94
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-03-08 00:14:40 -0800

    Moving Platform Added

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public int x;
-    float offset;
-    float initial;
+    //public int x;
+    //float offset;
+    //float initial;
+    public GameObject platform;
+    public float speed;
+    public Transform Point;
+    public Transform[] points;
+    public int pointSelection;
+
+    void Start()
+    {
+        Point = points[pointSelection];
+    }
 
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time, x), transform.position.y);
-
+        //transform.position = new Vector3(Mathf.PingPong(Time.time, x), transform.position.y);
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, Point.transform.position, Time.deltaTime * speed);
+        if(platform.transform.position == Point.position)
+        {
+            pointSelection++;
+            if(pointSelection == points.Length)
+            {
+                pointSelection = 0;
+            }
+            Point = points[pointSelection];
+        }
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -20,10 +39,9 @@ public class MovingPlatform : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Space))
             {
-                offset = GameObject.Find("Player").transform.position.x - transform.position.x;
+                //offset = GameObject.Find("Player").transform.position.x - transform.position.x;
                 other.transform.parent = null;
-                other.transform.position = new Vector3(transform.position.x + offset, other.transform.position.y);
-                
+                //other.transform.position = new Vector3(transform.position.x + offset, other.transform.position.y);
             }
             else
             {
