ec1c49eea6ca259ad744d876f95ae38515ce8233
jasonthaisy <jtsy@uci.edu>
2017-03-08 16:15:15 -0800

    Quick fix

--- Assets/BreakableActivateSpawn.cs
+++ Assets/BreakableActivateSpawn.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class BreakableActivateSpawn : MonoBehaviour {
     public GameObject other;
-    public float xPos;
-    public float yPos;
-    public float zPos;
-    public float xScale = 1;
-    public float yScale = 1;
-    public float zScale = 1;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            GameObject spawn = (GameObject)Instantiate(other, new Vector3(xPos, yPos, zPos), transform.rotation);
-            spawn.transform.localScale = new Vector3(xScale, yScale, zScale);
+            other.SetActive(true);
         }
     }
 }
