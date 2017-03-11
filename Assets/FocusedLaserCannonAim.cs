631fc19b6ed083abdef8ab8c5242fb1532140ada
kbuci <krishenb@uci.edu>
2017-03-11 12:01:40 -0800

    started work on continous laser cannon

---
+++ Assets/FocusedLaserCannonAim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusedLaserCannonAim : MonoBehaviour {
+
+    public GameObject target;
+    
+    private float rotation;
+    public GameObject focusLaser;
+	// Use this for initialization
+	void Start () {
+       
+        rotation = Vector2.Angle(gameObject.transform.position, target.gameObject.transform.position);
+        Debug.Log(rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
