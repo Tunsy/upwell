adf182dd7e13b82d73341cd3f0cf581a3726806f
kbuci <krishenb@uci.edu>
2017-03-11 16:23:44 -0800

    progress on laser                                      #proj.transform.localScale = new Vector3(1, Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)),  0)oroj.transform.localScale = new Vector3(1, Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)),  0);

--- Assets/FocusedLaserCannonAim.cs
+++ Assets/FocusedLaserCannonAim.cs
@@ -6,17 +6,26 @@ public class FocusedLaserCannonAim : MonoBehaviour {
 
     public GameObject target;
     
-    private float rotation;
+    private Quaternion rotation = Quaternion.identity;
+    private Vector3 midPoint;
     public GameObject focusLaser;
 	// Use this for initialization
 	void Start () {
-       
-        rotation = Vector2.Angle(gameObject.transform.position, target.gameObject.transform.position);
+        float ang = Vector2.Angle(gameObject.transform.position, target.gameObject.transform.position);
+        rotation = Quaternion.Euler(0, 0 , ang);
+        midPoint = new Vector3((transform.position.x + target.transform.position.y) / 2, (transform.position.y + target.transform.position.y) / 2);
         Debug.Log(rotation);
-	}
+        GameObject proj = GameObject.Instantiate(focusLaser);
+        
+        //Debug.Log(Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)));
+        proj.transform.localScale += new Vector3(1, Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)),  0);
+        proj.transform.position = midPoint;
+        proj.transform.rotation = rotation;
+    }
+    
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 }
