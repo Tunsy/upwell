2d695792c309009d846372783743a6aff99906b9
kbuci <krishenb@uci.edu>
2017-03-11 19:51:50 -0800

    laser stream cannon progress

--- Assets/FocusedLaserCannonAim.cs
+++ Assets/FocusedLaserCannonAim.cs
@@ -8,24 +8,43 @@ public class FocusedLaserCannonAim : MonoBehaviour {
     
     private Quaternion rotation = Quaternion.identity;
     private Vector3 midPoint;
+    public float chargeTime;
     public GameObject focusLaser;
+    public float timer;
 	// Use this for initialization
 	void Start () {
-        float ang = Vector2.Angle(gameObject.transform.position, target.gameObject.transform.position);
+        Vector2 diff = -transform.position  + target.transform.position;
+        float ang = Vector2.Angle(new Vector2(0, 1), diff);
+        if (diff.y < 0)
+        {
+            ang *= -1;
+        }
+        Debug.Log(ang );
         rotation = Quaternion.Euler(0, 0 , ang);
         midPoint = new Vector3((transform.position.x + target.transform.position.y) / 2, (transform.position.y + target.transform.position.y) / 2);
         Debug.Log(rotation);
-        GameObject proj = GameObject.Instantiate(focusLaser);
         
-        //Debug.Log(Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)));
-        proj.transform.localScale += new Vector3(1, Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)),  0);
-        proj.transform.position = midPoint;
-        proj.transform.rotation = rotation;
     }
     
 	
 	// Update is called once per frame
 	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= chargeTime)
+        {
+            timer = 0;
+            shootStream();
+        }
         
-	}
+    }
+
+    void shootStream()
+    {
+        GameObject proj = GameObject.Instantiate(focusLaser);
+        float scale = (float)1;
+        //Debug.Log(Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)));
+        proj.transform.localScale += new Vector3(0, Mathf.Sqrt(Mathf.Pow(transform.position.x - target.transform.position.x, 2) + Mathf.Pow(transform.position.y - target.transform.position.y, 2)) * scale, 0);
+        proj.transform.position = midPoint;
+        proj.transform.rotation = rotation;
+    }
 }
---
+++ Assets/LaserStream.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserStream : Projectile {
+
+	// Use this for initialization
+	void Start () {
+        speed = 0;
+        Invoke("DestroySelf", (float) 0.3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void  DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    public override void  OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
