b0eaaa74a9e6c7adee956a4a59fe69e5ca69639c
kbuci <krishenb@uci.edu>
2017-03-15 16:55:58 -0700

    modified rotation trap

--- Assets/Scripts/ChainPivot.cs
+++ Assets/Scripts/ChainPivot.cs
@@ -7,6 +7,7 @@ public class ChainPivot : MonoBehaviour {
     public int segments;
     public GameObject seg;
     public float radialSpeed;
+    public float radius;
 
  
     // Use this for initialization
@@ -15,6 +16,8 @@ public class ChainPivot : MonoBehaviour {
 		for(double i = 0; i < 360; i += 360/ segments)
         {
             GameObject segment = GameObject.Instantiate(seg);
+            segment.transform.localScale = new Vector3(seg.transform.localScale.x, radius, 0);
+            Debug.Log(seg.transform.localScale.x);
             segment.transform.position = new Vector2(this.transform.position.x,  this.transform.position.y-segment.transform.localScale.y);
             segment.transform.RotateAround(this.transform.position, Vector3.forward, (float)i );
             segment.transform.SetParent(this.transform);
