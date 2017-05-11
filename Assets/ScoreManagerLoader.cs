1612e1e6abef496385dddbeb6357afac0f1288a4
kbuci <krishenb@uci.edu>
2017-05-10 18:52:44 -0700

    added a ScoreManagerLoader (like GameManagerLoader)

---
+++ Assets/ScoreManagerLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerLoader : MonoBehaviour {
+
+    public GameObject scoreManager;          //GameManager prefab to instantiate.
+
+
+
+    void Awake()
+    {
+        //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        if (ScoreManager.instance == null)
+
+            //Instantiate gameManager prefab
+            Instantiate(scoreManager);
+
+    }
+}
