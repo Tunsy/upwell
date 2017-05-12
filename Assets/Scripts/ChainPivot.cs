ee37de5fc39d49864e4879c980c1a231d1e439d5
kbuci <krishenb@uci.edu>
2017-05-12 11:46:12 -0700

    fixed chain pivot  Please enter the commit message for your changes. Lines starting

--- Assets/Scripts/ChainPivot.cs
+++ Assets/Scripts/ChainPivot.cs
@@ -7,31 +7,36 @@ public class ChainPivot : MonoBehaviour {
     public int segments;
     public GameObject seg;
     public float radialSpeed;
-    public float diameter;
+    public float length;
 
  
     // Use this for initialization
 	void Start () {
 
-        float pivotRad = transform.localScale.y/2;
+        
 		for(double i = 0; i < 360; i += 360/ segments)
         {
             GameObject segment = GameObject.Instantiate(seg);
-            segment.transform.localScale = new Vector3(seg.transform.localScale.x, diameter, 0);
+            segment.transform.localScale = new Vector3(seg.transform.localScale.x, length, 0);
             
             //Debug.Log(seg.transform.localScale.x);
             
             //segment.transform.position = new Vector2(this.transform.position.x,  this.transform.position.y-segment.transform.localScale.y);
-            segment.transform.position = new Vector2(transform.position.x, transform.position.y - (pivotRad + diameter/2) );
+            //segment.transform.position = new Vector2(transform.position.x, transform.position.y -  );
             //segment.transform.position = new Vector2(transform.position.x, transform.position.y - segment.transform.position.y - segment.transform.localScale.y/2);
-            // segment.transform.RotateAround(this.transform.position, Vector3.forward, (float)i );
+            
             segment.transform.SetParent(this.transform);
+         
+            float y =segment.GetComponent<CapsuleCollider2D>().bounds.extents.y;
+            segment.transform.localPosition = new Vector2(0, -y);
+            segment.transform.RotateAround(this.transform.position, Vector3.forward, (float)i);
+
 
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.Rotate(Vector3.forward * Time.deltaTime * radialSpeed);
+        transform.Rotate(Vector3.forward * Time.deltaTime * radialSpeed);
 	}
 }
