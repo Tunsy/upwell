ff8b04e9bc7458b00771aa02475327bee761de72
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-06-02 11:42:21 -0700

    Pushing stuff so I can sync

---
+++ Assets/Totalstars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Totalstars : MonoBehaviour {
+
+    private int coins;
+    public Text text;
+    public GameObject manager;
+
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+        //Scene scene = SceneManager.GetActiveScene();
+        // level = scene.name;
+    }
+
+    void Update()
+    {
+        coins = manager.GetComponent<ScoreManager>().totalStars();
+        text.text = "Total: " + coins;
+    }
+}
