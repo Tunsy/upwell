430893557bd957f584609691cf59f3d09a8743fd
kbuci <krishenb@uci.edu>
2017-06-04 14:38:48 -0700

    added moving platfrom that starts upon being shot

---
+++ Assets/Shoot2Start.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot2Start : MonoBehaviour {
+    public float starting_speed;
+    private bool off = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(off && collision.gameObject.tag == "Bullit")
+        {
+            gameObject.GetComponent<MovingPlatform>().speed = starting_speed;
+        }
+    }
+}
